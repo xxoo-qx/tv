0276dbbd96877c166df60aab99537d813432f97d
XXOO <34995671+xxoo-qx@users.noreply.github.com>
2024-06-09 13:08:09 +0800

    Update itvallZHGXTV.py

--- itvallZHGXTV.py
+++ itvallZHGXTV.py
@@ -1,11 +1,14 @@
 import time
 import os
-import concurrent.futures
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import requests
 import re
-###urls城市根据自己所处地理位置修改
+import threading
+from queue import Queue
+import eventlet
+
+
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+
 urls = [
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0ic2hpamlhemh1YW5nIg%3D%3D",  #shijiazhuang 石家庄
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iaGFuZGFuIg%3D%3D",  # handan 邯郸
@@ -16,99 +19,26 @@ urls = [
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iemhhbmdqaWFrb3Ui",  # zhangjiakou 张家口
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iY2FuZ3pob3Ui",  # cangzhou 沧州
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0ibGFuZ2Zhbmci",  # langfang 廊坊
+
 ]
-
-def modify_urls(url):
-    modified_urls = []
-    ip_start_index = url.find("//") + 2
-    ip_end_index = url.find(":", ip_start_index)
-    base_url = url[:ip_start_index]  # http:// or https://
-    ip_address = url[ip_start_index:ip_end_index]
-    port = url[ip_end_index:]
-    ip_end = "/ZHGXTV/Public/json/live_interface.txt"
-    for i in range(1, 256):
-        modified_ip = f"{ip_address[:-1]}{i}"
-        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
-        modified_urls.append(modified_url)
-
-    return modified_urls
-
-
-def is_url_accessible(url):
-    try:
-        response = requests.get(url, timeout=1)
-        if response.status_code == 200:
-            return url
-    except requests.exceptions.RequestException:
-        pass
-    return None
-
 
 results = []
 
 for url in urls:
     try:
-        # 创建一个Chrome WebDriver实例
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-    
-        driver = webdriver.Chrome(options=chrome_options)
-        # 使用WebDriver访问网页
-        driver.get(url)  # 将网址替换为你要访问的网页地址
-        time.sleep(10)
-        # 获取网页内容
-        page_content = driver.page_source
-    
-        # 关闭WebDriver
-        driver.quit()
-    
+        response = requests.get(url, headers=headers, timeout=15)
+        page_content = response.text
         # 查找所有符合指定格式的网址
         pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
         urls_all = re.findall(pattern, page_content)
         # urls = list(set(urls_all))  # 去重得到唯一的URL列表
         urls = set(urls_all)  # 去重得到唯一的URL列表
-        x_urls = []
-        for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
-            url = url.strip()
-            ip_start_index = url.find("//") + 2
-            ip_end_index = url.find(":", ip_start_index)
-            ip_dot_start = url.find(".") + 1
-            ip_dot_second = url.find(".", ip_dot_start) + 1
-            ip_dot_three = url.find(".", ip_dot_second) + 1
-            base_url = url[:ip_start_index]  # http:// or https://
-            ip_address = url[ip_start_index:ip_dot_three]
-            port = url[ip_end_index:]
-            ip_end = "1"
-            modified_ip = f"{ip_address}{ip_end}"
-            x_url = f"{base_url}{modified_ip}{port}"
-            x_urls.append(x_url)
-        urls = set(x_urls)  # 去重得到唯一的URL列表
-    
-        valid_urls = []
-        #   多线程获取可用url
-        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-            futures = []
-            for url in urls:
-                url = url.strip()
-                modified_urls = modify_urls(url)
-                for modified_url in modified_urls:
-                    futures.append(executor.submit(is_url_accessible, modified_url))
-    
-            for future in concurrent.futures.as_completed(futures):
-                result = future.result()
-                if result:
-                    valid_urls.append(result)
-    
-        for url in valid_urls:
-            print(url)
         # 遍历网址列表，获取JSON文件并解析
-        for url in valid_urls:
+        for url in urls:
             try:
                 # 发送GET请求获取JSON文件，设置超时时间为0.5秒
-                json_url = f"{url}"
-                response = requests.get(json_url, timeout=1)
+                json_url = f'{url}/ZHGXTV/Public/json/live_interface.txt'
+                response = requests.get(json_url, timeout=5)
                 json_data = response.content.decode('utf-8')
                 try:
                     # 按行分割数据
@@ -116,14 +46,13 @@ for url in urls:
                     for line in lines:
                         line = line.strip()
                         if line:
-                            name, channel_url = line.split(',')
+                            name, channel_url = line.split(',',1)
                             urls = channel_url.split('/', 3)
                             url_data = json_url.split('/', 3)
                             if len(urls) >= 4:
                                 urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
                             else:
                                 urld = (f"{urls[0]}//{url_data[2]}")
-                            print(f"{name},{urld}")
                             if name and urld:
                                 # 删除特定文字
                                 name = name.replace("cctv", "CCTV")
@@ -159,6 +88,7 @@ for url in urls:
                                 name = name.replace("CCTV10科教", "CCTV10")
                                 name = name.replace("CCTV11戏曲", "CCTV11")
                                 name = name.replace("CCTV12社会与法", "CCTV12")
+                                name = name.replace("CCTV12法制", "CCTV12")
                                 name = name.replace("CCTV13新闻", "CCTV13")
                                 name = name.replace("CCTV新闻", "CCTV13")
                                 name = name.replace("CCTV14少儿", "CCTV14")
@@ -169,7 +99,9 @@ for url in urls:
                                 name = name.replace("CCTV5+体育赛视", "CCTV5+")
                                 name = name.replace("CCTV5+体育赛事", "CCTV5+")
                                 name = name.replace("CCTV5+体育", "CCTV5+")
-                                results.append(f"{name},{urld}")
+                                name = name.replace("河北少儿科教", "河北少儿")
+                                if "udp://" not in urld:
+                                    results.append(f"{name},{urld}")
                 except:
                     continue
             except:
@@ -179,7 +111,74 @@ for url in urls:
 
 results = set(results)   # 去重得到唯一的URL列表
 results = sorted(results)
-with open("itv.txt", 'w', encoding='utf-8') as file:
+
+def channel_key(channel_name):
+    match = re.search(r'\d+', channel_name)
+    if match:
+        return int(match.group())
+    else:
+        return float('inf')  # 返回一个无穷大的数字作为关键字
+
+# 对频道进行排序
+results.sort(key=lambda x: channel_key(x[0]))
+
+result_counter = 8  # 每个频道需要的个数
+
+with open("ds.txt", 'w', encoding='utf-8') as file:
+    channel_counters = {}
+    file.write('河北频道,#genre#\n')
     for result in results:
-        file.write(result + "\n")
-        print(result)
+        channel_name, channel_url = result.split(',',1)
+        if '河北' in channel_name or '石家庄' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+    channel_counters = {}
+    file.write('央视频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url = result.split(',',1)
+        if 'CCTV' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+
+    channel_counters = {}
+    file.write('卫视频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url = result.split(',',1)
+        if '卫视' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+    channel_counters = {}
+    file.write('其他频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url = result.split(',',1)
+        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name and '河北' not in channel_name and '石家庄' not in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
