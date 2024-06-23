9b31786c784be780df30aa6b16cfad7e94327227
XXOO <34995671+xxoo-qx@users.noreply.github.com>
2024-06-23 13:18:14 +0800

    Update itvallZHGXTV.py

--- itvallZHGXTV.py
+++ itvallZHGXTV.py
@@ -124,20 +124,20 @@ results.sort(key=lambda x: channel_key(x[0]))
 result_counter = 8  # 每个频道需要的个数
 
 with open("itv.txt", 'w', encoding='utf-8') as file:
-    channel_counters = {}
-    file.write('河北频道,#genre#\n')
-    for result in results:
-        channel_name, channel_url = result.split(',',1)
-        if '河北' in channel_name or '石家庄' in channel_name:
-            if channel_name in channel_counters:
-                if channel_counters[channel_name] >= result_counter:
-                    continue
-                else:
-                    file.write(f"{channel_name},{channel_url}\n")
-                    channel_counters[channel_name] += 1
-            else:
-                file.write(f"{channel_name},{channel_url}\n")
-                channel_counters[channel_name] = 1
+    # channel_counters = {}
+    # file.write('河北频道,#genre#\n')
+    # for result in results:
+    #     channel_name, channel_url = result.split(',',1)
+    #     if '河北' in channel_name or '石家庄' in channel_name:
+    #         if channel_name in channel_counters:
+    #             if channel_counters[channel_name] >= result_counter:
+    #                 continue
+    #             else:
+    #                 file.write(f"{channel_name},{channel_url}\n")
+    #                 channel_counters[channel_name] += 1
+    #         else:
+    #             file.write(f"{channel_name},{channel_url}\n")
+    #             channel_counters[channel_name] = 1
     channel_counters = {}
     file.write('央视频道,#genre#\n')
     for result in results:
